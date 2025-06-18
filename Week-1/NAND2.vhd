ce5c7ba64c58e6e3f5a569daab9408b9c49ef119
Krishna-get <krishnakukreja1357@gmail.com>
2025-06-18 18:12:35 +0530

    Create NAND2.vhd

---
+++ Week-1/NAND2.vhd
@@ -0,0 +1,16 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use work.NAND_pkg.all;
+
+entity NAND2 is
+  port(
+    A : in STD_LOGIC;
+    B : in STD_LOGIC;
+    Y : out STD_LOGIC
+  );
+end entity NAND2;
+
+architecture Behavioral of NAND2 is
+begin
+  Y <= not (A and B);
+end architecture Behavioral;
